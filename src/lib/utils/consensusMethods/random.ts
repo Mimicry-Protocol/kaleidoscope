f26640ecc6c3e683eb42c54943311aeac557a525
Aslan Goldenhour <aslan@mimicry.finance>
2023-07-01 17:55:19 +0300

    make random more defensive

--- src/lib/utils/consensusMethods/random.ts
+++ src/lib/utils/consensusMethods/random.ts
@@ -4,6 +4,9 @@ import { Value } from '../../types';
  * Return a random value from the list
  */
 export function random(values: Value[]): Value {
+  if (values.length === 0) {
+    throw new Error('Cannot select a random Value from an empty array');
+  }
   const randomIndex: number = Math.floor(Math.random() * values.length);
   return values[randomIndex];
 }
