b91b50ee0946d9966abf97920a36288a5295f4af
Aslan Goldenhour <aslan@mimicry.finance>
2023-07-04 07:25:58 +0300

    updated constructor to setup with DIA and CoinGecko if no config provided

--- src/lib/kaleidoscope/Kaleidoscope.ts
+++ src/lib/kaleidoscope/Kaleidoscope.ts
@@ -2,8 +2,8 @@ import { NftCollectionFactory } from './vendors/dataProviders/nftCollection/NftC
 import { FungibleFactory } from './vendors/dataProviders/fungibleToken/FungibleFactory';
 
 export class Kaleidoscope {
-  nftCollection?: NftCollectionFactory;
-  fungibleToken?: FungibleFactory;
+  nftCollection: NftCollectionFactory;
+  fungibleToken: FungibleFactory;
 
   constructor(_config: any) {
     if (__DEV__) {
@@ -17,11 +17,29 @@ export class Kaleidoscope {
     // Setup NFT Collection Data Providers
     if (_config.dataProviders.nfts) {
       this.nftCollection = new NftCollectionFactory(_config);
+    } else {
+      this.nftCollection = new NftCollectionFactory({
+        dataProviders: {
+            nfts: {
+                dia: true,
+                coinGecko: true,
+            },
+        },
+      });
     }
 
     // Setup Fungible Token Data Providers
     if (_config.dataProviders.fungibleTokens) {
       this.fungibleToken = new FungibleFactory(_config);
+    } else {
+      this.fungibleToken = new FungibleFactory({
+        dataProviders: {
+          fungibleTokens: {
+                dia: true,
+                coinGecko: true,
+            },
+        },
+      });
     }
   }
 }
